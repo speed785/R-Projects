7b742d5d759a7eeed179e4fd29b99218b3e0b081
James <ioandumitru59@gmail.com>
2019-06-10 17:35:37 -0500

    Added last of R projects

--- tst.R
+++ tst.R
@@ -17,3 +17,22 @@ lm.beta(reg)
 fitted(reg)
 
 cor(exlect)
+
+# independent 2-group t-test
+t.test(y~x) # where y is numeric and x is a binary factor
+
+# independent 2-group t-test
+t.test(x,y) # where y1 and y2 are numeric
+
+# paired t-test
+t.test(x,y,paired=TRUE) # where y1 & y2 are numeric
+
+# one sample t-test
+t.test(y,mu=3) # Ho: mu=3
+
+##########################################################part 2##########
+z <- c(6, 8, 10, 6, 7, 13, 5, 5, 3)
+exlect <- data.frame(z)
+
+# one sample t-test
+t.test(z,mu=3) # Ho: mu=3
